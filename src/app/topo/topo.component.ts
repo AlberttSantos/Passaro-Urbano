da61ad78e10103ed2535bdb16a217ced05f3757f
Albertt Santos <alberttaurelio@gmail.com>
2019-02-20 22:38:46 -0300

    Finalizando aulas de observable - Aula 160

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/ofertas.model'
-import { Observable } from 'rxjs'
+import { Observable, Subject, of } from 'rxjs'
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators'
 
 @Component({
   selector: 'app-topo',
@@ -14,15 +15,33 @@ export class TopoComponent implements OnInit {
   constructor(private ofertasService: OfertasService) { }
 
   public ofertas: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
+  public ofertas2: Oferta[]
 
   ngOnInit() {
-  }
- 
-  public pesquisa(termoDaPesquisa: string): void {
-    this.ofertas = this.ofertasService.pesquisaOfertas(termoDaPesquisa)    
+    this.ofertas = this.subjectPesquisa
+      .pipe(
+        debounceTime(1000), //Executa a requisição apos 1 segundo
+        distinctUntilChanged(), //Se o termo for diferente do anterior executa outra requisição
+        switchMap((termoBuscado: string) => {
+          console.log("Requisição http")
 
-    this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas),
-    (erro: any) => console.log("Erro Ocorrido" + erro),
-    () => console.log("Fluxo de enventos completo")    
-    )}
+          if (termoBuscado.trim() === '') {
+            return of<Oferta[]>([])
+          }
+
+          return this.ofertasService.pesquisaOfertas(termoBuscado)
+        }))
+    catchError((erro) => {
+      console.log(erro)
+      return of<Oferta[]>([])
+    })
+
+    this.ofertas.subscribe((oferta: Oferta[]) => this.ofertas2 = oferta)
+  }
+
+  public pesquisa(termoDaPesquisa: string): void {
+    console.log("keyup", termoDaPesquisa)
+    this.subjectPesquisa.next(termoDaPesquisa)
+  }
 }
